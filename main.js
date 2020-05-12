7405d7c8c190317154dc2bacb8556414fdadfcd5
Hugo Dutka <contact@hugodutka.com>
2020-05-12 15:10:00 +0200

    6+7.6

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
 var el = document.querySelector("input[type=submit]");
 el.style.display = "none";
 var p = document.querySelector("#pierwszy");
@@ -28,3 +65,93 @@ console.log(maxIdPassenger);
 setTimeout(function () {
     console.log("No już wreszcie.");
 }, 2000);
+var wait = function (ms) {
+    return new Promise(function (r) { return setTimeout(r, ms); });
+};
+var teczoweKolory = function (el) { return __awaiter(_this, void 0, void 0, function () {
+    var colors, _i, colors_1, c;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                colors = [
+                    "red",
+                    "orange",
+                    "yellow",
+                    "green",
+                    "blue",
+                    "indigo",
+                    "purple",
+                ];
+                _i = 0, colors_1 = colors;
+                _a.label = 1;
+            case 1:
+                if (!(_i < colors_1.length)) return [3 /*break*/, 4];
+                c = colors_1[_i];
+                return [4 /*yield*/, wait(1000)];
+            case 2:
+                _a.sent();
+                console.log(c);
+                el.style.backgroundColor = c;
+                _a.label = 3;
+            case 3:
+                _i++;
+                return [3 /*break*/, 1];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+teczoweKolory(document.querySelector(".passengers"));
+var displayCommiterInfo = function () { return __awaiter(_this, void 0, void 0, function () {
+    var container;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                container = document.querySelector("#commiter-info");
+                return [4 /*yield*/, fetch("https://api.github.com/repos/Microsoft/TypeScript/commits")
+                        .then(function (response) {
+                        return response.json()["catch"](function (e) {
+                            if (!response.ok) {
+                                console.log("failed to fetch commit data", e);
+                                return null;
+                            }
+                        });
+                    })
+                        .then(function (json) {
+                        if (json === null || json.length === 0)
+                            return;
+                        var username = json[0].author.login;
+                        var avatarUrl = json[0].author.avatar_url;
+                        var img = document.createElement("img");
+                        img.setAttribute("src", avatarUrl);
+                        container.appendChild(img);
+                        return fetch("https://api.github.com/users/" + username + "/repos");
+                    })
+                        .then(function (response) {
+                        return response.json()["catch"](function (e) {
+                            if (!response.ok) {
+                                console.log("failed to fetch repository data", e);
+                                return null;
+                            }
+                        });
+                    })
+                        .then(function (repos) {
+                        if (repos === null)
+                            return;
+                        repos.sort(function (a, b) {
+                            return a.full_name.toLowerCase().localeCompare(b.full_name.toLowerCase());
+                        });
+                        for (var _i = 0, repos_1 = repos; _i < repos_1.length; _i++) {
+                            var _a = repos_1[_i], full_name = _a.full_name, html_url = _a.html_url;
+                            var div = document.createElement("div");
+                            var link = document.createElement("a");
+                            div.appendChild(link);
+                            link.setAttribute("href", html_url);
+                            link.innerText = full_name;
+                            container.appendChild(div);
+                        }
+                    })];
+            case 1: return [2 /*return*/, _a.sent()];
+        }
+    });
+}); };
+displayCommiterInfo();
--- main.ts
+++ main.ts
@@ -39,3 +39,73 @@ console.log(maxIdPassenger);
 setTimeout(() => {
   console.log("No już wreszcie.");
 }, 2000);
+
+const wait = (ms: number): Promise<void> =>
+  new Promise((r) => setTimeout(r, ms));
+
+const teczoweKolory = async (el: HTMLElement) => {
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+  ];
+  for (const c of colors) {
+    await wait(1000);
+    console.log(c);
+    el.style.backgroundColor = c;
+  }
+};
+
+teczoweKolory(document.querySelector(".passengers"));
+
+const displayCommiterInfo = async (): Promise<void> => {
+  const container: HTMLElement = document.querySelector("#commiter-info");
+  return await fetch(
+    "https://api.github.com/repos/Microsoft/TypeScript/commits"
+  )
+    .then((response) =>
+      response.json().catch((e) => {
+        if (!response.ok) {
+          console.log("failed to fetch commit data", e);
+          return null;
+        }
+      })
+    )
+    .then((json) => {
+      if (json === null || json.length === 0) return;
+      const username = json[0].author.login;
+      const avatarUrl = json[0].author.avatar_url;
+      const img = document.createElement("img");
+      img.setAttribute("src", avatarUrl);
+      container.appendChild(img);
+      return fetch(`https://api.github.com/users/${username}/repos`);
+    })
+    .then((response) =>
+      response.json().catch((e) => {
+        if (!response.ok) {
+          console.log("failed to fetch repository data", e);
+          return null;
+        }
+      })
+    )
+    .then((repos) => {
+      if (repos === null) return;
+      repos.sort((a, b) =>
+        a.full_name.toLowerCase().localeCompare(b.full_name.toLowerCase())
+      );
+      for (const { full_name, html_url } of repos) {
+        const div = document.createElement("div");
+        const link = document.createElement("a");
+        div.appendChild(link);
+        link.setAttribute("href", html_url);
+        link.innerText = full_name;
+        container.appendChild(div);
+      }
+    });
+};
+
+displayCommiterInfo();
