87778b21bf7a59866c8768d38e81af23424fbb1f
Hugo Dutka <contact@hugodutka.com>
2020-05-12 14:22:03 +0200

    6+7.4

---
+++ main.js
@@ -0,0 +1,27 @@
+var el = document.querySelector("input[type=submit]");
+el.style.display = "none";
+var p = document.querySelector("#pierwszy");
+console.log(p);
+var newElem = document.createElement("p");
+newElem.innerHTML = "Jestem nowym akapitem!";
+document.querySelector("body").appendChild(newElem);
+var passengers = document.querySelectorAll(".passenger");
+var passId = "data-identyfikator-pasazera";
+for (var _i = 0, passengers_1 = passengers; _i < passengers_1.length; _i++) {
+    var passenger = passengers_1[_i];
+    passenger.setAttribute(passId, Math.floor(Math.random() * Math.pow(10, 6)).toString());
+}
+var maxIdPassenger = null;
+for (var _a = 0, passengers_2 = passengers; _a < passengers_2.length; _a++) {
+    var passenger = passengers_2[_a];
+    if (maxIdPassenger === null) {
+        maxIdPassenger = passenger;
+    }
+    else if (maxIdPassenger.getAttribute(passId) >= passenger.getAttribute(passId)) {
+        continue;
+    }
+    else {
+        maxIdPassenger = passenger;
+    }
+}
+console.log(maxIdPassenger);
---
+++ main.ts
@@ -0,0 +1,37 @@
+let el = document.querySelector("input[type=submit]") as HTMLInputElement;
+el.style.display = "none";
+
+let p = document.querySelector("#pierwszy") as HTMLParagraphElement;
+console.log(p);
+
+let newElem = document.createElement("p") as HTMLParagraphElement;
+newElem.innerHTML = "Jestem nowym akapitem!";
+document.querySelector("body").appendChild(newElem);
+
+let passengers = document.querySelectorAll(".passenger") as NodeListOf<
+  HTMLElement
+>;
+
+const passId = "data-identyfikator-pasazera";
+
+for (let passenger of passengers) {
+  passenger.setAttribute(
+    passId,
+    Math.floor(Math.random() * 10 ** 6).toString()
+  );
+}
+
+var maxIdPassenger: null | HTMLElement = null;
+for (let passenger of passengers) {
+  if (maxIdPassenger === null) {
+    maxIdPassenger = passenger;
+  } else if (
+    maxIdPassenger.getAttribute(passId) >= passenger.getAttribute(passId)
+  ) {
+    continue;
+  } else {
+    maxIdPassenger = passenger;
+  }
+}
+
+console.log(maxIdPassenger);
